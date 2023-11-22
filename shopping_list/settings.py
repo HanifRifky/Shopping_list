17d29e291c2980107bec283cbf89c26e2eb3ab5d
HanifRifky <114400903+HanifRifky@users.noreply.github.com>
2023-11-22 16:04:55 +0700

    Tutorial 9 (Redeploy4)

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -144,7 +144,6 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
-#CSRF_TRUSTED_ORIGINS = ["https://muhamad-hanif21-tutorial.pbp.cs.ui.ac.id"]
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://muhamad-hanif21-tutorial.pbp.cs.ui.ac.id"]
+#CSRF_TRUSTED_ORIGINS = ["*"]
 
-#test
